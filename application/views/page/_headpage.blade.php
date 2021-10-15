719962bb6b10eb1d7b7a641c0527bd406efe45f5
syifaauliarahma <19200255@bsi.ac.id>
2021-10-15 17:20:52 +0700

    Menambahkan HeadPage agar bisa di lihat di semua page

---
+++ application/views/page/_headpage.blade.php
@@ -0,0 +1,54 @@
+<div class="row">
+  <div class="col-lg-3">
+    <div class="ibox ">
+      <div class="ibox-title">
+        <span class="label label-success float-right"></span> <span class="label label-success float-right"></span> <span class="label label-success float-right"></span>
+        <h5>Jumlah Pengguna</h5>
+      </div>
+      <div class="ibox-content">
+        <h1 class="no-margins">400</h1>
+        <div class="stat-percent font-bold text-success">50% <i class="fa fa-bolt"></i></div>
+        <small>Pengguna </small>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3">
+    <div class="ibox ">
+      <div class="ibox-title">
+        <span class="label label-info float-right"></span><span class="label label-info float-right"></span><span class="label label-info float-right"></span>
+        <h5>Pendaftar Layanan</h5>
+      </div>
+      <div class="ibox-content">
+        <h1 class="no-margins">30</h1>
+        <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
+        <small>Pengguna </small>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3">
+    <div class="ibox ">
+      <div class="ibox-title">
+        <span class="label label-primary float-right">Hari Ini</span>
+        <h5>Pendaftar Layanan </h5>
+      </div>
+      <div class="ibox-content">
+        <h1 class="no-margins">6</h1>
+        <div class="stat-percent font-bold text-navy">5% <i class="fa fa-level-up"></i></div>
+        <small>Pengguna </small>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3">
+    <div class="ibox ">
+      <div class="ibox-title">
+        <span class="label label-danger float-right">Hari Besok</span>
+        <h5>Pendaftar Layanan</h5>
+      </div>
+      <div class="ibox-content">
+        <h1 class="no-margins">20</h1>
+        <div class="stat-percent font-bold text-danger">15% <i class="fa fa-level-down"></i></div>
+        <small>Pengguna </small>
+      </div>
+    </div>
+  </div>
+</div>
