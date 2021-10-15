fcc5e6fab5053f608f5b91cac57ba5e4a6d7519a
syifaauliarahma <19200255@bsi.ac.id>
2021-10-15 17:20:29 +0700

    Merubah Tampilan Dashboard

--- application/views/page/_dashboard.blade.php
+++ application/views/page/_dashboard.blade.php
@@ -1,4 +1,12 @@
 @extends('master')
 @section('content')
-<h1>Ini Halaman Untuk Konten</h1>
+@include('page._headpage')
+<div class="row">
+  <div class="col-lg-12">
+    <h5>Grafik Penggunaan Layanan Pemasyarakatan</h5>
+    <div class="flot-chart m-b-xl">
+      <div class="flot-chart-content" id="flot-dashboard5-chart"></div>
+    </div>
+  </div>
+</div>
 @endsection
