f5197af4dbcaa2f8d119e55778f98c96fdff9e26
Adilman Zai <kangadilzai27051999@gmail.com>
2021-10-18 22:34:26 +0700

    Membuat Page Tentang Aplikasi

--- application/views/page/admin/_cetak_ticket.blade.php
+++ application/views/page/admin/_cetak_ticket.blade.php
@@ -1,22 +1,46 @@
 @extends('master')
 @section('content')
-<div class="wrapper wrapper-content animated fadeInRight">
-  <div class="row">
-    <div class="col-lg-12" align="center" >
-      <div class="ibox ">
-        <div class="col-sm-6 b-r" >
-          <div class="m-b-md">
-            <img alt="image" src="<?php echo base_url('assets/img/angular_logo.png')?>">
-          </div>
-          <h4>Silahkan Masukan Kode Ticket Anda !</h4>
-          <form class="" role="form" action="index.html">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Masukan Kode Ticket Disini" required="">
+<div class="row">
+  <div class="col-lg-12" align="center">
+    <div class="ibox ">
+      <div class="ibox-content">
+        <div class="col-md-12">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/angular_logo.png')?>">
+              </div>
+              <div class="carousel-item">
+                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/meteor_logo.png')?>">
+              </div>
+              <div class="carousel-item">
+                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/starter_logo.jpg')?>">
+              </div>
             </div>
-            <button type="submit" class="btn btn-primary block">Cari</button>
-          </form>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
         </div>
       </div>
+      <h4>Silahkan Masukan Kode Ticket Anda !</h4>
+      <form class="col-md-6" role="form" action="index.html">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Masukan Kode Ticket Disini" required="">
+        </div>
+        <button type="submit" class="btn btn-primary block">Cari</button>
+      </form>
     </div>
   </div>
 </div>
