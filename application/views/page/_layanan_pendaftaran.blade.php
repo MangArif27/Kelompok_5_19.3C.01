4a1282191d5e8a98499fa46d5501459b3270b7ad
Adilman Zai <kangadilzai27051999@gmail.com>
2021-11-03 19:52:16 +0700

    Merubah Read pada page layanan pendaftaran

--- application/views/page/_layanan_pendaftaran.blade.php
+++ application/views/page/_layanan_pendaftaran.blade.php
@@ -28,6 +28,7 @@
               <tbody>
                 <?php $no=1; ?>
                 @foreach($layanan as $dtlayanan)
+                @if($dtlayanan->no_identitas==$_SESSION['no_identitas'])
                 <tr class="gradeX">
                   <td><?php echo $no++ ?></td>
                   <td><?php echo $dtlayanan->nama_wbp ?></td>
@@ -38,6 +39,7 @@
                     <button class="btn <?php echo $dtlayanan->button ?> btn-rounded" href="#"><i class="fa fa-eye"></i> <?php echo $dtlayanan->status_layanan ?></button>
                   </td>
                 </tr>
+                @endif
                 @endforeach
               </tbody>
             </table>
@@ -54,85 +56,82 @@ $check=$CI->m_user->search_user($NoIdentitas);
 if($check->num_rows() == 1){
   foreach($check->result() as $data){
     ?>
-<div class="modal inmodal" id="Tambah" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content animated fadeIn">
-      <div class="modal-header">
-        <h5 class="modal-title"> Formulir Layanan Pendaftaran</h5>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="contact-box">
-              <div class="row">
-                <div class="col-12">
-                  <form class="m-t" role="form" enctype="multipart/form-data" method="post" action="Proses/Tambah/Pengguna">
-                    <div class="form-group">
-                      <input type="text" class="form-control col-12" name="NoIdentitas" value="<?php echo $data->no_identitas; ?>" readonly>
-                    </div>
-                    <div class="form-group">
-                      <input type="text" class="form-control col-12" name="Nama"  value="<?php echo $data->nama; ?>" readonly>
-                    </div>
-                    <div class="form-group">
-                      <textarea class="form-control col-12" name="Alamat" placeholder="Alamat" readonly><?php echo $data->alamat; ?></textarea>
-                    </div>
-                    <div class="form-group">
-                      <select class="form-control m-b" name="JenisKelamin" readonly>
-                        @if($data->jenis_kelamin == "Laki-Laki")
-                        <option value="Laki-Laki" selected>Laki-Laki</option>
-                        <option value="Perempuan">Perempuan</option>
-                        @else
-                        <option value="Laki-Laki" >Laki-Laki</option>
-                        <option value="Perempuan" selected>Perempuan</option>
-                        @endif
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <input type="text" class="form-control col-12" name="NoHandphone" value="<?php echo $data->no_handphone; ?>" readonly>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <div class="contact-box">
-                <div class="row">
-                  <div class="col-12">
-                    <div class="form-group">
-                      <input type="email" class="form-control col-12" name="Email" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <label>Photo Diri :</label>
-                      <input type="file" class="form-control col-12" name="Photo" placeholder="Photo">
-                    </div>
-                    <div class="form-group">
-                      <label>Scan Identitas :</label>
-                      <input type="file" class="form-control col-12" name="ScanIdentitas" placeholder="Photo">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control col-12" name="Password" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <select class="form-control m-b" name="Level">
-                        <option value="Admin">Admin</option>
-                        <option value="User">User</option>
-                        <option value="Pending">Pending</option>f
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div class="modal inmodal" id="Tambah" tabindex="-1" role="dialog"  aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content animated fadeIn">
+          <div class="modal-header">
+            <h5 class="modal-title"> Formulir Layanan Pendaftaran</h5>
           </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="contact-box">
+                  <div class="row">
+                    <div class="col-12">
+                      <form class="m-t" role="form" enctype="multipart/form-data" method="post" action="Proses/Tambah/Pendaftaran">
+                        <div class="form-group">
+                          <input type="text" class="form-control col-12" name="NoIdentitas" value="<?php echo $data->no_identitas; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <input type="text" class="form-control col-12" name="Nama"  value="<?php echo $data->nama; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                          <textarea class="form-control col-12" name="Alamat" placeholder="Alamat" readonly><?php echo $data->alamat; ?></textarea>
+                        </div>
+                        <div class="form-group">
+                          <select class="form-control m-b" name="JenisKelamin" readonly>
+                            @if($data->jenis_kelamin == "Laki-Laki")
+                            <option value="Laki-Laki" selected>Laki-Laki</option>
+                            <option value="Perempuan">Perempuan</option>
+                            @else
+                            <option value="Laki-Laki" >Laki-Laki</option>
+                            <option value="Perempuan" selected>Perempuan</option>
+                            @endif
+                          </select>
+                        </div>
+                        <div class="form-group">
+                          <input type="text" class="form-control col-12" name="NoHandphone" value="<?php echo $data->no_handphone; ?>" readonly>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="contact-box">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="form-group">
+                          <select data-placeholder="~ Pilih Warga Binaan ~" class="chosen-select form-control"  tabindex="2" name="NoInduk">
+                            <option disabled selected>~ Pilih Warga Binaan ~</option>
+                            @foreach($CI->m_datawbp->tmpl_datawbp()->result() as $dtwbp)
+                            <option value="<?= $dtwbp->no_induk ?>"><?= $dtwbp->nama_wbp; ?></option>
+                            @endforeach
+                          </select>
+                        </div>
+                      </div>
+                      <div class="form-group col-12">
+                        <input type="date" class="form-control" name="TglPelaksanaan" id="TglPelaksanaan" format="YYYY-mm-dd">
+                      </div>
+                      <div class="form-group col-12" >
+                        <select data-placeholder="~ Pilih Jenis Layanan~" class="chosen-select form-control"  tabindex="2" name="JenisLayanan" id="JenisLayanan">
+                          <option disabled selected>~ Pilih Jenis Layanan ~</option>
+                          <option value="Video Call">Video Call</option>
+                          <option value="Penitipan Barang">Penitipan Barang</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+          </form>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Simpan</button>
-        </div>
-      </form>
+      </div>
     </div>
-  </div>
-</div>
-<?php } }
-?>
-@endsection
+  <?php } }
+  ?>
+  @endsection
