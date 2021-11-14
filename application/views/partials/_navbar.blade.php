e93dac6207f3f21ec2a48e26675c4dc44c507769
muarif27 <muarif130912@gmail.com>
2021-11-14 14:45:31 +0700

    Menghapus tampilan selamat datang

--- application/views/partials/_navbar.blade.php
+++ application/views/partials/_navbar.blade.php
@@ -5,7 +5,6 @@
     </div>
     <ul class="nav navbar-top-links navbar-right">
       <li style="padding: 20px">
-        <span class="m-r-sm text-muted welcome-message">Selamat Datang di Layanan Keluarga WBP</span>
       </li>
       <li>
         <a href="Proses/Logout">
