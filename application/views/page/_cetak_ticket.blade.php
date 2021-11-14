81dd52198ab2821cb91ee561777a324e44028cc1
muarif27 <muarif130912@gmail.com>
2021-11-14 22:07:53 +0700

    Membuat Slide Gambar dan Form Submit Tiket

--- application/views/page/_cetak_ticket.blade.php
+++ application/views/page/_cetak_ticket.blade.php
@@ -9,17 +9,13 @@
             <ol class="carousel-indicators">
               <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
               <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/angular_logo.png')?>">
+                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/images/slide/Slide1.png')?>" height="150px">
               </div>
               <div class="carousel-item">
-                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/meteor_logo.png')?>">
-              </div>
-              <div class="carousel-item">
-                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/img/starter_logo.jpg')?>">
+                <img class="col-md-10" alt="image" src="<?php echo base_url('assets/images/slide/Slide2.jpg')?>" height="150px">
               </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -34,10 +30,23 @@
 
         </div>
       </div>
+      <?php $CI = get_instance(); ?>
+      @if($CI->session->flashdata('message_tiket_error'))
+      <div class="alert alert-danger col-md-12 alert-dismissable">
+        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+        <a class="alert-link" href="#"><?= $CI->session->flashdata('message_tiket_error') ?></a>.
+      </div>
+      @elseif($CI->session->flashdata('message_tiket_success'))
+      <div class="alert alert-success col-md-12 alert-dismissable">
+        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+        <a class="alert-link" href="#"><?= $CI->session->flashdata('message_tiket_success');  ?></a>.
+      </div>
+      @else
+      @endif
       <h4>Silahkan Masukan Kode Ticket Anda !</h4>
-      <form class="col-md-6" role="form" action="index.html">
+      <form class="col-md-6" role="form" action="Proses/Search/Tiket-Layanan" method="post">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Masukan Kode Ticket Disini" required="">
+          <input type="text" class="form-control" name="tiket" placeholder="Masukan Kode Ticket Disini" required="">
         </div>
         <button type="submit" class="btn btn-primary block">Cari</button>
       </form>
