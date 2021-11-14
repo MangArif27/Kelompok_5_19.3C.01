6f3481af571a145d51dbd6d3261f90b3f9d2b8eb
muarif27 <muarif130912@gmail.com>
2021-11-14 14:46:06 +0700

    Membuat page login yang berikan tentang aplikasi secara dinamis tampil dari database

--- application/views/partials/_login.blade.php
+++ application/views/partials/_login.blade.php
@@ -18,19 +18,10 @@
       @else
       @endif
       <div class="col-md-6">
-        <h2 class="font-bold">Selamat Datang Di Sistem Informasi Layanan Pemasyarakatan</h2>
-        <p>
-          Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
-        </p>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-        </p>
-        <p>
-          When an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        </p>
-        <p>
-          <small>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</small>
-        </p>
+        @foreach($about as $abt)
+        <h2 class="font-bold">{{$abt->nama_apk}}</h2>
+        {!! $abt->tentang_apk !!}
+        @endforeach
       </div>
       <div class="col-md-6">
         <div class="ibox-content">
