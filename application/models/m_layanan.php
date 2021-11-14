931618e493b6b50de8f15276e48304799cba4257
muarif27 <muarif130912@gmail.com>
2021-11-14 22:07:33 +0700

    Membuat Seacrh Tiket dengan InnerJoin, dan Update Tiket

--- application/models/m_layanan.php
+++ application/models/m_layanan.php
@@ -18,13 +18,26 @@ class m_layanan extends CI_Model{
     $this->db->where($NoInduk);
     $this->db->update($this->table,$data);
   }
-  function search_datapendaftaran($Tiket){
-    $sql = $this->db->query("SELECT * FROM $this->table WHERE  tiket = '$Tiket'");
+  function search_datapendaftaran($tiket){
+    $sql = $this->db->query("SELECT * FROM $this->table WHERE  tiket = '$tiket'");
     return $sql;
   }
   function delete_datapendaftaran($Tiket){
     $this->db->where($Tiket);
     $this->db->delete($this->table, $Tiket);
+  }
+  function seacrh_tiket($tiket){
+    $this->db->select('*');
+    $this->db->where('tiket', $tiket);
+    $this->db->from('layanan');
+    $this->db->join('datawbp', 'datawbp.no_induk=layanan.no_induk', 'LEFT');
+    $this->db->join('user', 'user.no_identitas=layanan.no_identitas', 'LEFT');
+    $query = $this->db->get();
+    return $query;
+  }
+  function update_tiket($tiket,$data){
+    $this->db->where('tiket',$tiket);
+    $this->db->update($this->table,$data);
   }
 }
 ?>
