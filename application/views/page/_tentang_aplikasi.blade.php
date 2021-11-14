5d3fb2f52e9d4b24c055c384b0e89e026ac0ce3e
muarif27 <muarif130912@gmail.com>
2021-11-14 14:29:01 +0700

    Membuat Create Read dan Update data About

--- application/views/page/_tentang_aplikasi.blade.php
+++ application/views/page/_tentang_aplikasi.blade.php
@@ -14,30 +14,50 @@
       <div class="contact-box">
         <div class="row">
           <div class="col-12">
-            <input type="text" class="form-control col-12" placeholder="Nama Aplikasi">
-            <br>
-            <input type="text" class="form-control col-12" placeholder="Nama Unit Pelaksana Teknis">
-            <br>
-            <textarea class="form-control col-12" placeholder="Alamat"></textarea>
-            <br>
-            <input type="number" class="form-control col-12" placeholder="Nomor Handphone">
-            <br>
-            <input type="email" class="form-control col-12" placeholder="Email">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-8">
-      <div class="contact-box">
-          <div class="col-lg-12">
-            <div class="ibox ">
-              <div class="ibox-content no-padding">
-                <div class="summernote">
-                </div>
+            @foreach($about as $abt)
+            <form class="m-t" role="form" enctype="multipart/form-data" method="post" action="Proses/Update/Tentang-Aplikasi/<?= $abt->id_upt; ?>">
+              <div class="form-group">
+                <input type="text" class="form-control col-12" name="Nama_Apk" placeholder="Nama Aplikasi" value="<?= $abt->nama_apk; ?>">
+              </div>
+              <div class="form-group">
+                <input type="number" class="form-control col-12" name="Id_Upt" value="<?= $abt->id_upt; ?>" readonly placeholder="Id Upt">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control col-12" name="Nama_Upt" placeholder="Nama Unit Pelaksana Teknis" value="<?= $abt->nama_upt; ?>">
+              </div>
+              <div class="form-group">
+                <textarea class="form-control col-12" name="Alamat" placeholder="Alamat"><?= $abt->alamat; ?></textarea>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control col-12" name="No_Telp" value="<?= $abt->no_tlp; ?>" placeholder="Nomor Telphon">
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control col-12" name="Email"value="<?= $abt->email; ?>" placeholder="Email">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control col-12" name="Website" value="<?= $abt->website; ?>" placeholder="Website">
               </div>
             </div>
           </div>
+        </div>
       </div>
-    </div>
+      <div class="col-lg-8">
+        <div class="ibox">
+          <div class="col-lg-12">
+            <div class="ibox-title">
+              <div class="ibox-content no-padding">
+                <textarea name="summernote" class="summernote">{{ $abt->tentang_apk }}</textarea>
+                @endforeach
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </form>
   </div>
-  @endsection
+</div>
+@endsection
